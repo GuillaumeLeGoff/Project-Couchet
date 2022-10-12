5f61aa90c4b7fbec7cab8a3eb0fff27a50af9307
GuillaumeLeGoff <legoff.guill@gmail.com>
2022-10-12 10:42:54 +0200

    first PUT

--- src/components/column/Truck.js
+++ src/components/column/Truck.js
@@ -1,163 +1,194 @@
-
-
-import Table from 'react-bootstrap/Table';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import Table from "react-bootstrap/Table";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 import axios from "axios";
 import { FaArrowUp, FaSave } from "react-icons/fa";
 import { useImmer } from "use-immer";
-import '../../styles/Main.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React, { useEffect } from 'react';
-import Come from '../State/Come';
-import Loading from '../State/Loading';
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons"
-    rel="stylesheet"></link>
-
-
+import "../../styles/Main.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useEffect } from "react";
+import Come from "../State/Come";
+import Loading from "../State/Loading";
+<link
+  href="https://fonts.googleapis.com/icon?family=Material+Icons"
+  rel="stylesheet"
+></link>;
 
 function Truck() {
-    const URL_API = "http://localhost:4000"
-    useEffect(() => {
-        getTruck();
-    }, []);
-    var [LoadingTruck, setLoadingTruck] = useImmer([]);
-    var [NextTruck, setNextTruck] = useImmer([]);
+  const URL_API = "http://localhost:4000";
+  useEffect(() => {
+    getTruck();
+  }, []);
+  var [LoadingTruck, setLoadingTruck] = useImmer([]);
+  var [NextTruck, setNextTruck] = useImmer([]);
 
-    function HandleToggleNext(e, docks) {
-        const id = docks.id
-        setNextTruck((draft) => {
-            const dock = draft.find((dock) => dock.id === id);
-            dock.plate = e.target.value;
-        });
-    }
-    function HandleToggleNextDock(e, docks) {
-        const id = docks.id
-        setNextTruck((draft) => {
-            const dock = draft.find((dock) => dock.id === id);
-            dock.dockIndex = e.target.value;
-        });
-    }
+  function HandleToggleNext(e, docks) {
+    const id = docks.id;
+    setNextTruck((draft) => {
+      const dock = draft.find((dock) => dock.id === id);
+      dock.plate = e.target.value;
+    });
+  }
+  function HandleToggleNextDock(e, docks) {
+    const id = docks.id;
+    setNextTruck((draft) => {
+      const dock = draft.find((dock) => dock.id === id);
+      dock.dockIndex = e.target.value;
+    });
+  }
 
-    function HandleToggleLoading(e, docks) {
-        const id = docks.id
-        setLoadingTruck((draft) => {
-            const dock = draft.find((dock) => dock.id === id);
-            dock.plate = e.target.value;
-        });
-    }
-    function HandleToggleLoadingDock(e, docks) {
-        const id = docks.id
-        setLoadingTruck((draft) => {
-            const dock = draft.find((dock) => dock.id === id);
-            dock.dockIndex = e.target.value;
-        });
-    }
+  function HandleToggleLoading(e, docks) {
+    const id = docks.id;
+    setLoadingTruck((draft) => {
+      const dock = draft.find((dock) => dock.id === id);
+      dock.plate = e.target.value;
+    });
+  }
+  function HandleToggleLoadingDock(e, docks) {
+    const id = docks.id;
+    setLoadingTruck((draft) => {
+      const dock = draft.find((dock) => dock.id === id);
+      dock.dockIndex = e.target.value;
+    });
+  }
 
-    function MoveLoadingToNext(e, docks) {
-        var id = docks.id;
-        var id2 = docks.id;
-        id2 = id2 - 6;
-         setNextTruck((draft) => {
-            const dock = draft.find((dock) => dock.id === id);
-            dock.plate = ""
-            dock.dockIndex = ""
-        }); 
+  function MoveLoadingToNext(e, docks) {
+    var id = docks.id;
+    var id2 = docks.id;
+    id2 = id2 - 6;
+    setNextTruck((draft) => {
+      const dock = draft.find((dock) => dock.id === id);
+      dock.plate = "";
+      dock.dockIndex = "";
+    });
 
-        setLoadingTruck((draft) => {
-            const dock = draft.find((dock) => dock.id === id2);
-            dock.plate = NextTruck[id2].plate;
-            dock.dockIndex = NextTruck[id2].dockIndex;
-            dock.state =  <Come/>;
-            dock.state =  <Loading/>;
-        }); 
-    }
-    async function getTruck() {
-        const data = {
-        };
-        await axios
-            .get(
-                URL_API + "/trucks",
-                JSON.stringify(data)
-            )
-            .then((result) => {
+    setLoadingTruck((draft) => {
+      const dock = draft.find((dock) => dock.id === id2);
+      dock.plate = NextTruck[id2].plate;
+      dock.dockIndex = NextTruck[id2].dockIndex;
+      dock.state = <Come />;
+      dock.state = <Loading />;
+    });
+  }
+  async function getTruck() {
+    const data = {};
+    await axios
+      .get(URL_API + "/trucks", JSON.stringify(data))
+      .then((result) => {
+        setLoadingTruck(result.data.slice(0, 6));
+        setNextTruck(result.data.slice(6, 12));
+        console.log(result.data.slice(6, 12));
+        console.log(result.data.slice(0, 6));
+      });
+  }
+  async function postTruck() {
+    axios
+      .put(
+        URL_API + "/truck/63456792d63187cba68d3ffa",
+        {
+          id: 85,
+          dockIndex: 100,
+          plate: "test",
+          state: true,
+        },
+        {
+          params: {
+            TruckId: "63456792d63187cba68d3ffa",
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res);
+        console.log(res.data);
+      });
+  }
 
-                setLoadingTruck(result.data.slice(0, 6));
-                setNextTruck(result.data.slice(6, 12));
-                console.log(result.data.slice(6, 12));
-                console.log(result.data.slice(0, 6));
-            });
-    }
-    async function postTruck() {
-        const data = {
-            data: {
-                id: 0,
-                dockIndex: 13,
-                plate: 'test',
-                state: 'come',
-            }
-        };
-        console.log(data);
-        axios.post(URL_API + "/trucks:0", { data })
-            .then(res => {
-                console.log(res);
-                console.log(res.data);
-            });
-    }
+  return (
+    <div className="Truck">
+      <Form>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Docks</th>
+              <th>Truck</th>
+              <th>State</th>
+            </tr>
+          </thead>
 
-
-    return (
-
-        <div className="Truck">
-            <Form >
-                <Table striped bordered hover>
-                    <thead>
-                        <tr>
-                            <th>Docks</th>
-                            <th>Truck</th>
-                            <th>State</th>
-                        </tr>
-                    </thead>
-
-                    {LoadingTruck.map(docks => (
-                        <tbody>
-                            <tr>
-                                <td> <Form.Control className="Inputtruck" onChange={(e) => HandleToggleLoadingDock(e, docks)} value={docks.dockIndex}/>  </td>
-                                <td><Form.Control className="Inputtruck" onChange={(e) => HandleToggleLoading(e, docks)} value={docks.plate} /></td>
-                                <td>{docks.state}</td>
-                            </tr>
-                        </tbody>
-                    ))}
-                </Table>
-                <Table striped bordered hover>
-                    <thead>
-                        <tr>
-                            <th>Docks</th>
-                            <th>Truck</th>
-                            <th>State</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    {NextTruck.map(docks => (
-                        <tbody>
-                            <tr>
-                            <td> <Form.Control className="Inputtruck" onChange={(e) => HandleToggleNextDock(e, docks)} value={docks.dockIndex}/>  </td>
-                                <td><Form.Control className="Inputtruck" onChange={(e) => HandleToggleNext(e, docks)} value={docks.plate} /></td>
-                                <td>{docks.state}</td>
-                                <td><Button className="ButtonUp" button onClick={(e) => MoveLoadingToNext(e, docks)} variant="primary" ><FaArrowUp /></Button></td>
-
-                            </tr>
-                        </tbody>
-                    ))}
-                </Table>
-                <Button className='FullScreenSave' variant="primary" type="submit" ><FaSave /></Button>
-            </Form>
-            <Button onClick={postTruck} ><FaSave /></Button>
-
-
-
-        </div>
-    );
+          {LoadingTruck.map((docks) => (
+            <tbody>
+              <tr>
+                <td>
+                  {" "}
+                  <Form.Control
+                    className="Inputtruck"
+                    onChange={(e) => HandleToggleLoadingDock(e, docks)}
+                    value={docks.dockIndex}
+                  />{" "}
+                </td>
+                <td>
+                  <Form.Control
+                    className="Inputtruck"
+                    onChange={(e) => HandleToggleLoading(e, docks)}
+                    value={docks.plate}
+                  />
+                </td>
+                <td>{docks.state}</td>
+              </tr>
+            </tbody>
+          ))}
+        </Table>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Docks</th>
+              <th>Truck</th>
+              <th>State</th>
+              <th></th>
+            </tr>
+          </thead>
+          {NextTruck.map((docks) => (
+            <tbody>
+              <tr>
+                <td>
+                  {" "}
+                  <Form.Control
+                    className="Inputtruck"
+                    onChange={(e) => HandleToggleNextDock(e, docks)}
+                    value={docks.dockIndex}
+                  />{" "}
+                </td>
+                <td>
+                  <Form.Control
+                    className="Inputtruck"
+                    onChange={(e) => HandleToggleNext(e, docks)}
+                    value={docks.plate}
+                  />
+                </td>
+                <td>{docks.state}</td>
+                <td>
+                  <Button
+                    className="ButtonUp"
+                    button
+                    onClick={(e) => MoveLoadingToNext(e, docks)}
+                    variant="primary"
+                  >
+                    <FaArrowUp />
+                  </Button>
+                </td>
+              </tr>
+            </tbody>
+          ))}
+        </Table>
+        <Button className="FullScreenSave" variant="primary" type="submit">
+          <FaSave />
+        </Button>
+      </Form>
+      <Button onClick={postTruck}>
+        <FaSave />
+      </Button>
+    </div>
+  );
 }
 
 export default Truck;
--- src/components/modeScreen/TimeScreen.js
+++ src/components/modeScreen/TimeScreen.js
@@ -1,110 +1,125 @@
 import { Button, Form, Table } from "react-bootstrap";
-import { FaSave } from 'react-icons/fa';
+import { FaSave } from "react-icons/fa";
 import { useImmer } from "use-immer";
 import { useRef } from "react";
 function MultiScreen() {
+  const dragItem = useRef();
+  const dragOverItem = useRef();
+  ///////////////////////DATA//////////////////////
+  var [MultiScreen, setMultiScreen] = useImmer([
+    {
+      id: 0,
+      fileName: "écran camion",
+      file: "salut",
+      Time: 4,
+    },
+  ]);
+  /////////////////////////////////////////////////
 
-    const dragItem = useRef();
-    const dragOverItem = useRef();
-    ///////////////////////DATA//////////////////////
-    var [MultiScreen, setMultiScreen] = useImmer([
-        {
-            id: 0,
-            fileName: "écran camion",
-            file: "salut",
-            Time: 4,
-        },
-    ]);
-    /////////////////////////////////////////////////
+  //////////////////DRAG AND DROP//////////////////
+  function dragStart(e, position) {
+    dragItem.current = position;
+    /* console.log(e.target.innerHTML); */
+  }
 
-    //////////////////DRAG AND DROP//////////////////
-    function dragStart(e, position) {
-        dragItem.current = position;
-        /* console.log(e.target.innerHTML); */
-    };
+  function dragEnter(e, position) {
+    dragOverItem.current = position;
+    /* console.log(e.target.innerHTML); */
+  }
 
-    function dragEnter(e, position) {
-        dragOverItem.current = position;
-        /* console.log(e.target.innerHTML); */
-    };
+  function drop(e) {
+    const copyListItems = [...MultiScreen];
+    const dragItemContent = copyListItems[dragItem.current];
+    copyListItems.splice(dragItem.current, 1);
+    copyListItems.splice(dragOverItem.current, 0, dragItemContent);
+    dragItem.current = null;
+    dragOverItem.current = null;
+    setMultiScreen(copyListItems);
+  }
+  ////////////////////////////////////////////////
 
-    function drop(e) {
-        const copyListItems = [...MultiScreen];
-        const dragItemContent = copyListItems[dragItem.current];
-        copyListItems.splice(dragItem.current, 1);
-        copyListItems.splice(dragOverItem.current, 0, dragItemContent);
-        dragItem.current = null;
-        dragOverItem.current = null;
-        setMultiScreen(copyListItems);
-    };
-    ////////////////////////////////////////////////
+  //////////////////OPTION FILE///////////////////
+  function NewFile() {
+    setMultiScreen((draft) => {
+      draft.push({
+        id: "file_" + Math.random(),
+        fileName: "new file" + Math.random(),
+        file: "new file",
+        Time: 1,
+      });
+    });
+  }
 
-    //////////////////OPTION FILE///////////////////
-    function NewFile() {
-        setMultiScreen((draft) => {
-            draft.push({
-                id: "file_" + Math.random(),
-                fileName: "new file" + Math.random(),
-                file: "new file",
-                Time: 1,
-            })
-        })
-    }
+  function DeleteFile(e, file) {
+    const id = file.id;
 
-    function DeleteFile(e, file) {
-        const id = file.id
+    setMultiScreen((draft) => {
+      const file1 = draft.findIndex((file1) => file1.id === id);
+      draft.splice(file1, 1);
+    });
+  }
 
-        setMultiScreen((draft) => {
-            const file1 = draft.findIndex((file1) => file1.id === id);
-            draft.splice(file1, 1);
-        })
-    }
+  function HandleToggleTime(e, file) {
+    const id = file.id;
+    setMultiScreen((draft) => {
+      const files = draft.find((files) => files.id === id);
+      files.Time = e.target.value;
+    });
+    console.log(file);
+  }
+  /////////////////////////////////////////////////
 
-    function HandleToggleTime(e, file) {
-        const id = file.id
-        setMultiScreen((draft) => {
-            const files = draft.find((files) => files.id === id);
-            files.Time = e.target.value;
-
-        });
-        console.log(file);
-
-    }
-    /////////////////////////////////////////////////
-
-    return (
-        <div>
-            <Form>
-                <Table striped bordered hover>
-                    <thead>
-                        <tr>
-                            <th>Nom</th>
-                            <th>Aperçu</th>
-                            <th>Durée</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    {MultiScreen &&
-                        MultiScreen.map((file, index) => (
-                            <tbody>
-                                <tr onDragStart={(e) => dragStart(e, index)}
-                                    onDragEnter={(e) => dragEnter(e, index)}
-                                    onDragEnd={drop}
-                                    key={index}
-                                    draggable>
-                                    <td key={file.id}>{file.fileName}</td>
-                                    <td> <Form.Control type="file" name="file" /> </td>
-                                    <td><Form.Control onChange={(e) => HandleToggleTime(e, file)} type="number" value={file.Time} min="1" /></td>
-                                    <td><Button onClick={(e) => DeleteFile(e, file)}>X</Button></td>
-                                </tr>
-                            </tbody>
-                        ))}
-                </Table>
-            </Form>
-            <Button variant="primary" type="submit" onClick={(e) => NewFile()}>Ajouter Document</Button>
-            <Button className='FullScreenSave' variant="primary" type="submit" ><FaSave /></Button>
-        </div >
-    );
+  return (
+    <div>
+      <Form>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Nom</th>
+              <th>Aperçu</th>
+              <th>Durée</th>
+              <th></th>
+            </tr>
+          </thead>
+          {MultiScreen &&
+            MultiScreen.map((file, index) => (
+              <tbody>
+                <tr
+                  onDragStart={(e) => dragStart(e, index)}
+                  onDragEnter={(e) => dragEnter(e, index)}
+                  onDragEnd={drop}
+                  key={index}
+                  draggable
+                >
+                  <td key={file.id}>{file.fileName}</td>
+                  <td>
+                    {" "}
+                    <Form.Control type="file" name="file" />{" "}
+                  </td>
+                  <td>
+                    <Form.Control
+                      onChange={(e) => HandleToggleTime(e, file)}
+                      type="number"
+                      value={file.Time}
+                      min="1"
+                    />
+                  </td>
+                  <td>
+                    <Button onClick={(e) => DeleteFile(e, file)}>X</Button>
+                  </td>
+                </tr>
+              </tbody>
+            ))}
+        </Table>
+      </Form>
+      <Button variant="primary" type="submit" onClick={(e) => NewFile()}>
+        Ajouter Document
+      </Button>
+      <Button className="FullScreenSave" variant="primary" type="submit">
+        <FaSave />
+      </Button>
+    </div>
+  );
 }
 
 export default MultiScreen;
